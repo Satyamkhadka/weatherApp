f7cc5f472dc1c89d1f18ed26922b22adc9b02ff8
Satyam Khadka <Satyam.khadka123@gmail.com>
2019-03-16 00:13:18 +0545

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ window.addEventListener('load',()=>{
         navigator.geolocation.getCurrentPosition(position=>{
            long = position.coords.longitude;
            lat=position.coords.latitude; 
-        const proxy= 'http://cors-anywhere.herokuapp.com/';
-           const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-           fetch(api).then(response=>{
+        //const proxy= 'http://cors-anywhere.herokuapp.com/';
+           //const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+            fetch(api).then(response=>{
                return response.json();
            }).then(data=>{
             console.log(data);
